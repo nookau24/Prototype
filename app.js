c0cccc56c45dedac4ee69e39a2bbc4f040344fcb
Noor Kaur <nookau24@bergen.org>
2023-02-01 18:56:28 -0500

    lab 2 v2

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
+//set up the server
+const express = require( "express" );
+const app = express();
+const logger = require("morgan");
+const port = 80;
+
+// define middleware that logs all incoming requests
+app.use(logger("dev"));
+
+// define middleware that serves static resources in the public directory
+app.use(express.static(__dirname + '/public'));
+
 // define a route for the default home page
 app.get( "/", ( req, res ) => {
     console.log(`${req.method} ${req.url}`);
-    next();
     res.sendFile( __dirname + "/views/index.html" );
-    const logger = require("morgan")
-    app.use(logger("dev"));
-    // define middleware that serves static resources in the public directory
-app.use(express.static(__dirname + '/public'));
 } );
 
 // define a route for the stuff inventory page
-app.get( "/page3/item", ( req, res ) => {
-    res.sendFile( __dirname + "/views/page3.html" );
+app.get( "/inventory", ( req, res ) => {
+    console.log(`${req.method} ${req.url}`);
+    res.sendFile( __dirname + "/views/inventory.html" );
 } );
 
 // define a route for the item detail page
-app.get( "/page2", ( req, res ) => {
-    res.sendFile( __dirname + "/views/item.html" );
+app.get( "/inventory/todos", ( req, res ) => {
+    console.log(`${req.method} ${req.url}`);
+    res.sendFile( __dirname + "/views/todos.html" );
+} );
+
+// start the server
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
 } );
